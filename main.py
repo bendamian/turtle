971f1d35cd617368b5f60a4681c858f6d5db824e
bendamian <badamian@gmail.com>
2023-11-14 21:27:02 +0000

    testing 5

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from turtle import Turtle, Screen
+import random
 
 tita = Turtle()
 tita.shape("turtle")
@@ -27,16 +28,34 @@ def dashed_line():
 def draw_shape():
     x = 3
     while x < 9:
-
+        arr = ["red", "tan2", "yellow", "lime"]
+        c = random.choice(arr)
         for _ in range(x):
+            tita.pencolor(c)
             tita.pensize(3)
             tita.forward(100)
             tita.right(360 / x)
         x += 1
 
 
+def random_walk():
+    arr = ["red", "tan2", "yellow", "lime"]
+    directions = [0,90, 180, 270]
+    tita.pensize(5)
+    tita.speed(0)
+    x = 0
+    while x < 250:
+        rd = random.choice(directions)
+        col = random.choice(arr)
+        tita.pencolor(col)
+        tita.forward(30)
+        tita.setheading(rd)
+        x += 1
+
+
 # dashed_line()
 # square(x=100)
 # draw_shape()
+#random_walk()
 screen = Screen()
 screen.exitonclick()
